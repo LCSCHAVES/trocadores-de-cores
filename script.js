0d71cb4ee912693f0531ce3fb0967dbc7c2ade52
Lucas <lucaschaves_190699@hotmail.com>
2024-10-23 21:59:09 -0300

    primeiro commit - todos os arquivos

---
+++ script.js
@@ -0,0 +1,12 @@
+ window.onload = function verm(){
+    let red = document.getElementById("cor"); 
+    red.style.backgroundcolor = 'red';
+}
+function pret(){
+    let black = document.getElementById("cor")
+    black.style.backgroundcolor = "black";
+}
+function azul(){
+    let blue = document.getElementById("cor");
+    blue.style.backgroundcolor = "blue";
+}
